3edaede9b995a4516cc9cad85f59d51e060811db
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2024-11-19 23:28:54 +0600

    adapter

--- app/src/main/java/com/example/dictionaryapp/MeaningAdapter.kt
+++ app/src/main/java/com/example/dictionaryapp/MeaningAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.dictionaryapp
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dictionaryapp.databinding.MeaningRecyclerRowBinding
@@ -10,6 +11,29 @@ class MeaningAdapter(private var meaningList : List<Meaning>) : RecyclerView.Ada
     class MeaningViewHolder( private  val binding: MeaningRecyclerRowBinding) : RecyclerView.ViewHolder(binding.root){
         fun bind(meaning: Meaning){
             binding.partOfSpeechTextview.text = meaning.partOfSpeech
+            binding.definitionsTextview.text =  meaning.definitions.joinToString("\n\n") {
+                var currentIndex = meaning.definitions.indexOf(it)
+                (currentIndex+1).toString()+". "+it.definition.toString()
+            }
+
+            if(meaning.synonyms.isEmpty()){
+                binding.synonymsTitleTextview.visibility = View.GONE
+                binding.synonymsTextview.visibility = View.GONE
+            }else{
+                binding.synonymsTitleTextview.visibility = View.VISIBLE
+                binding.synonymsTextview.visibility = View.VISIBLE
+                binding.synonymsTextview.text = meaning.synonyms.joinToString(", ")
+            }
+
+            if(meaning.antonyms.isEmpty()){
+                binding.antonymsTitleTextview.visibility = View.GONE
+                binding.antonymsTextview.visibility = View.GONE
+            }else{
+                binding.antonymsTitleTextview.visibility = View.VISIBLE
+                binding.antonymsTextview.visibility = View.VISIBLE
+                binding.antonymsTextview.text = meaning.antonyms.joinToString(", ")
+            }
+
         }
     }
 
